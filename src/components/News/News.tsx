462d8ea9b3416d2c92e991abeea1533bcb8d89ad
Lonerr7 <krabik2342@gmail.com>
2023-10-09 12:14:56 +0300

    12. Made first version of select in News page

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import { fetchCryptoNews } from '../../redux/slices/newsSlice';
 import s from './News.module.scss';
 import Preloader from '../common/Preloader/Preloader';
 import NewsCard from './NewsCard/NewsCard';
+import Select, { SingleValue } from 'react-select';
+import { getCryptos } from '../../redux/slices/coinsSlice';
 
 interface Props {
   simplified?: boolean;
@@ -11,27 +13,47 @@ interface Props {
 
 const News: React.FC<Props> = ({ simplified }) => {
   const news = useAppSelector((state) => state.news.news);
-  const isFetching = useAppSelector((state) => state.news.isFetching);
+  const cryptos = useAppSelector((state) => state.coins.coins);
+  const isNewsFetching = useAppSelector((state) => state.news.isFetching);
   const dispatch = useAppDispatch();
 
-  console.log(news);
+  const [selectOption, setSelectOption] = useState('Cryptocurrency');
 
   const itemsCount = simplified ? 6 : 100;
+  const selectOptions = cryptos?.map((coin, index) => {
+    if (index === 0) {
+      return {
+        label: 'Cryptocurrency',
+        value: 'Cryptocurrency',
+      };
+    }
+
+    return {
+      label: coin.name,
+      value: coin.name,
+    };
+  });
+
+  const onSelectChange = (newValue: any) => {
+    setSelectOption(newValue?.value);
+  };
 
   useEffect(() => {
     dispatch(
       fetchCryptoNews({
-        newsCategory: 'Cryptocurrency',
+        newsCategory: selectOption,
         count: itemsCount,
       })
     );
 
     // eslint-disable-next-line
-  }, []);
+  }, [selectOption]);
 
-  if (isFetching) {
-    return <Preloader />;
-  }
+  useEffect(() => {
+    dispatch(getCryptos(100));
+
+    // eslint-disable-next-line
+  }, []);
 
   const newsCards = news?.map((newsItem, i) => (
     <NewsCard
@@ -46,7 +68,28 @@ const News: React.FC<Props> = ({ simplified }) => {
     />
   ));
 
-  return <div className={s.news}>{newsCards}</div>;
+  return (
+    <div
+      className={s.news}
+      style={
+        simplified
+          ? {
+              padding: 0,
+            }
+          : {}
+      }
+    >
+      <Select
+        className={s.news__select}
+        options={selectOptions}
+        onChange={onSelectChange}
+        defaultInputValue={'Cryptocurrency'}
+      />
+      <div className={s.news__inner}>
+        {isNewsFetching ? <Preloader /> : newsCards}
+      </div>
+    </div>
+  );
 };
 
 export default News;
